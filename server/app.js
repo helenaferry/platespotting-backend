3f476dba17634a0ef8c737a4cbdb68f96072d76e
Helena Ferry <helena.ferry>
2020-09-25 09:04:28 +0200

    Trying allow origin localhost

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ router.use(bodyParser.json());
 
 // CORS middleware
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8081/');
     res.header('Access-Control-Allow-Methods', '*');
 	res.header('Access-Control-Allow-Headers', 'content-type');
     next();
